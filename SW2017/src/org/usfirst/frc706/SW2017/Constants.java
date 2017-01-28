2ff896c0634d977440c3bcc403c79b1a8db2a4ca
Aaron Wasserman <8wasserman@gmail.com>
2017-01-28 16:51:31 -0600

    Implemented Constants
    Implemented constants for speed controller ports and solenoid channels.

--- SW2017/src/org/usfirst/frc706/SW2017/Constants.java
+++ SW2017/src/org/usfirst/frc706/SW2017/Constants.java
@@ -1,5 +1,41 @@
 package org.usfirst.frc706.SW2017;
 
 public class Constants {
-
+	public class Chassis {
+		public static final int LEFT_ONE = 0;
+		public static final int LEFT_TWO = 1;
+		public static final int RIGHT_ONE = 2;
+		public static final int RIGHT_TWO = 3;
+		public static final int WINCH_ONE = 4;
+		public static final int WINCH_TWO = 5;
+		public static final int SHIFTER_FORWARD = 0;
+		public static final int SHIFTER_REVERSE = 1;
+	}
+	public class Shooter {
+		public static final int CONVEYOR = 6;
+		public static final int AGITATOR = 7;
+		public static final int SHOOTER_LEFT = 8;
+		public static final int SHOOTER_RIGHT = 9;
+	}
+	public class Intake {
+		public static final int INTAKE = 10;
+	}
+	public class Gear {
+		public static final int RELEASE_FORWARD = 2;
+		public static final int RELEASE_REVERSE = 3;
+		public static final int RECEIVE_FORWARD = 4;
+		public static final int RECEIVE_REVERSE = 5;
+	}
+	public class OI {
+		public static final int LEFT_JOY = 0;
+		public static final int RIGHT_JOY = 1;
+		public static final int XBOX = 2;
+		public static final int TRIGGER = 1;
+	}
+	public class Autonomous {
+		
+	}
+	public class PCM {
+		public static final int PCM_ONE = 0;
+	}
 }
--- SW2017/src/org/usfirst/frc706/SW2017/RobotMap.java
+++ SW2017/src/org/usfirst/frc706/SW2017/RobotMap.java
@@ -13,23 +13,23 @@ public class RobotMap {
     public static CANTalon shooterLeftShooter, shooterRightShooter;
     public static CANTalon intakeIntakeMotor;
     public static DoubleSolenoid gearReleaseFlap;
-    public static Solenoid chassisShiftSol;
-    public static Solenoid gearReceiveFlap;
+    public static DoubleSolenoid chassisShiftSol;
+    public static DoubleSolenoid gearReceiveFlap;
 
     public static void init() {
-        chassisLeftDriveOne = new CANTalon(0); 
-        chassisLeftDriveTwo = new CANTalon(1);
-        chassisRightDriveOne = new CANTalon(2);
-        chassisRightDriveTwo = new CANTalon(3);
-        chassisWinchOne = new CANTalon(4);
-        chassisWinchTwo = new CANTalon(5);
-        chassisShiftSol = new Solenoid(0, 1);
-        shooterConveyorMotor = new CANTalon(6);
-        shooterAgitatorMotor = new CANTalon(7);
-        shooterLeftShooter = new CANTalon(8);
-        shooterRightShooter = new CANTalon(9);
-        intakeIntakeMotor = new CANTalon(10);
-        gearReleaseFlap = new DoubleSolenoid(0, 2, 3);
-        gearReceiveFlap = new Solenoid(0, 4);
+        chassisLeftDriveOne = new CANTalon(Constants.Chassis.LEFT_ONE); 
+        chassisLeftDriveTwo = new CANTalon(Constants.Chassis.LEFT_TWO);
+        chassisRightDriveOne = new CANTalon(Constants.Chassis.RIGHT_ONE);
+        chassisRightDriveTwo = new CANTalon(Constants.Chassis.RIGHT_TWO);
+        chassisWinchOne = new CANTalon(Constants.Chassis.WINCH_ONE);
+        chassisWinchTwo = new CANTalon(Constants.Chassis.WINCH_TWO);
+        shooterConveyorMotor = new CANTalon(Constants.Shooter.CONVEYOR);
+        shooterAgitatorMotor = new CANTalon(Constants.Shooter.AGITATOR);
+        shooterLeftShooter = new CANTalon(Constants.Shooter.SHOOTER_LEFT);
+        shooterRightShooter = new CANTalon(Constants.Shooter.SHOOTER_RIGHT);
+        intakeIntakeMotor = new CANTalon(Constants.Intake.INTAKE);
+        chassisShiftSol = new DoubleSolenoid(Constants.PCM.PCM_ONE, Constants.Chassis.SHIFTER_FORWARD, Constants.Chassis.SHIFTER_REVERSE);
+        gearReleaseFlap = new DoubleSolenoid(Constants.PCM.PCM_ONE, Constants.Gear.RELEASE_FORWARD, Constants.Gear.RELEASE_REVERSE);
+        gearReceiveFlap = new DoubleSolenoid(Constants.PCM.PCM_ONE, Constants.Gear.RECEIVE_FORWARD, Constants.Gear.RECEIVE_REVERSE);
     }
 }
